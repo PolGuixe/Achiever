556f1bc1a3cd5140f93a97e9f0c3f510815075a8
Sophie Dundovic <sophie.dundovic@gmail.com>
2014-02-08 20:16:15 +0000

    updated index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-        <title>Stop Start by FS </title>
+        <title>Achiever</title>
     </head>
     <body>
 		<div class="panel intro">
@@ -25,33 +25,42 @@
 				<a href="http://standrewsfashion.co.uk/"><img src="img/fs.png" style="width: 30%; float: left; margin: 10%;"></a>
 				<a href="http://www.theyachtweek.com/"><img src="img/tyw.png" style="width: 40%; float: left; margin: 5%; margin-top: 10%;"></a>
 				<div style="width: 100%; clear: both; text-align: center">
-					THE OFFICIAL FS AFTERPARTY<br/>
-					15.02.14 // 10.30PM<br/>
-					LOWER COLLEGE LAWN
+					Achieve your goals<br/>
+					Be who you want to be<br/>
+					<br> Making the latest discoveries in behavioural science work for you </br> 
 				</div>
+
 			</div>
 		</div>
 		<div class="vidpanel">
 			
 		</div>
 		<div class="panel artist3">
-			<span class="artistname">John Christian</span>
+			<span class="artistname">It is as easy as 1,2,3...</span>
 			<div class="artistdesc">
-				John Christian is a DJ, producer, and recording studio owner based in the Netherlands. With Nicky Romero John began his foray into dance music championing his first release, a remix of Stevie Wonder classic Superstition. John Christian then went on to remix I Could Be The One, Romero’s wildly successful collaboration with Avicii. Since this hit remix, John Christian has continued releasing tracks on Tiesto's musical freedom label, including his very popular track flight 463, which has had support from the biggest names in the industry.
+				<p> Choose your goal, set your stake, get going! </p>
+				<p> I want to .... <p>
+					<ul>
+						<li> Exercise 3 times this week </li> 
+						<li> Wake up early </li>
+					</ul>
+
+
+
 			</div>
 			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/113341777&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
 		<div class="panel artist1">
-			<span class="artistname">A.C.B</span>
+			<span class="artistname">Login</span>
 			<div class="artistdesc">
-				Since Summer 2011 A.C.B has set the St Andrews music scene alight, playing at the the biggest and best events in St Andrews as well as clubs in Edinburgh and London, including the world famous Ministry of Sound. For the past year A.C.B has been the head of music at the St Andrews Charity Fashion Show, headlining STARFIELDS festival during freshers week and composing the mix for the show itself. Included in the show will be a preview of his debut release on Papa records, Sababu.
+				Login with Facebook
 			</div>
 			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/125630357&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
 		<div class="panel artist2">
-			<span class="artistname">Stanislaw</span>
+			<span class="artistname">Set your stake</span>
 			<div class="artistdesc">
-				After co-founding moodroom collective in 2010, Henry was head of music at FS in 2012 and 2013. Now in London, he is forging a music career, working as an A&R Scout for Imagem Music Publishing and continuing to DJ, with his London debut at Unleash this December.
+				<p> I stake 
 			</div>
 			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/117786642&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
