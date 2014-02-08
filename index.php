76bf26777dd13f94ef4f63cfaa37b65c6d138088
PolGuixe <pol.guixe@gmail.com>
2014-02-08 20:29:36 +0000

    division classes name updates

--- index.php
+++ index.php
@@ -22,8 +22,6 @@
 		<div class="panel intro">
 			<div class="imgholder">
 				<img src="img/logo.png">
-				<a href="http://standrewsfashion.co.uk/"><img src="img/fs.png" style="width: 30%; float: left; margin: 10%;"></a>
-				<a href="http://www.theyachtweek.com/"><img src="img/tyw.png" style="width: 40%; float: left; margin: 5%; margin-top: 10%;"></a>
 				<div style="width: 100%; clear: both; text-align: center">
 					Achieve your goals<br/>
 					Be who you want to be<br/>
@@ -32,10 +30,7 @@
 
 			</div>
 		</div>
-		<div class="vidpanel">
-			
-		</div>
-		<div class="panel artist3">
+		<div class="chooseGoal">
 			<span class="artistname">It is as easy as 1,2,3...</span>
 			<div class="artistdesc">
 				<p> Choose your goal, set your stake, get going! </p>
@@ -48,37 +43,28 @@
 
 
 			</div>
-			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/113341777&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
-		<div class="panel artist1">
+		<div class="logFace">
 			<span class="artistname">Login</span>
 			<div class="artistdesc">
 				Login with Facebook
 			</div>
-			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/125630357&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
+			
 		</div>
-		<div class="panel artist2">
+		<div class="setStake">
 			<span class="artistname">Set your stake</span>
 			<div class="artistdesc">
 				<p> I stake 
 			</div>
-			<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/117786642&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
-		<div class="panel artist4">
+		<div class="chooseReferee">
 			<span class="artistname">AliMog</span>
 			<div class="artistdesc">
-				Ali came on to the scene in 2011 with a residency at Ransom, Edinburgh. Ali is currently a resident at 'Vanity' & 'Mansion' in Aberdeen, as well as having previously played at St Andrews Rugby 7' & Opening Ball 2012.
+				.....
 			</div>
-			<iframe width="100%" height="250" src="//www.mixcloud.com/widget/iframe/?feed=http%3A%2F%2Fwww.mixcloud.com%2FAlimog%2Fbig-room-prog-house-mix%2F&amp;embed_type=widget_standard&amp;embed_uuid=c7644f41-6019-4473-ab6a-5fecff23a157&amp;hide_tracklist=1&amp;hide_cover=1" frameborder="0"></iframe>
 		</div>
-		<div class="panel info">
-			TICKET SALES<br/>
-			£20<br/>
-			FRIDAY 7TH FEBRUARY<br/>
-			2PM<br/>
-			VENUE 1 - UNION<br/>
-			LIMITED CAPACITY<br/>
-			<a href="https://www.facebook.com/events/638029862899421/">FIND THE EVENT ON FACEBOOK HERE</a>
+		<div class="congrats">
+			
 		</div>
     </body>
 </html>
