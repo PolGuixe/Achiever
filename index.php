626defc7ea01b02899edf4dfba8c6bbaa56dfdcd
JesalR <jesalrajdev@Jesals-MacBook-Pro.local>
2014-02-09 00:56:32 +0000

    moved main.js

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
         <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
         <!--<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>-->
         <script src="js/jquery.datetimepicker.js"></script>
-        <script src="js/main.js"></script>
         
         <title>Achiever</title>
     </head>
