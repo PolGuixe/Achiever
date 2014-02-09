0f6c2b8ce79ea50dd86f2f3f499e2c3362995c82
JesalR <jesalrajdev@Jesals-MacBook-Pro.local>
2014-02-09 10:32:16 +0000

    removed inline FB code to min.'s

--- js/main.js
+++ js/main.js
@@ -37,6 +37,24 @@ function isValidEmailAddress(emailAddress) {
 };
 
 $(window).load(function() {
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1418604408384387',
+      status     : true,
+      xfbml      : true
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
 	$("#fb_login").click(function(){
 		logIn();
 	});
